19fae255cd989ccc4bea0dc0638a96addb4270db
Stephen J. Butler <stephen.butler@gmail.com>
2018-12-03 14:42:07 -0600

    Allow docker to build w/o mapping source volume

--- docker/centos7/build.sh
+++ docker/centos7/build.sh
@@ -1,5 +1,9 @@
 #!/bin/bash
 set -xe
 
+if [[ ! -e /source/.git ]]; then
+    git clone -b $UIUC_SHIBPLUGINS_GITBRANCH $UIUC_SHIBPLUGINS_GITURL /source/
+fi
+
 cmake3 -DUIUC_SHIBPLUGINS_INSTALL_DIR:PATH=/output /source
 make
